44410d6875b88b124efc6d53dac62f03339f2be1
radov02 <radovicevic.erik1@gmail.com>
2024-03-28 20:39:46 +0100

    New commit

--- vaje/vaje05/naloga.c
+++ vaje/vaje05/naloga.c
@@ -4,39 +4,15 @@
 
 #include "naloga.h"
 
-<<<<<<< HEAD
 int vsota(int* zac, int* kon) {
     int vsota = 0;
     for(int i = *zac; i <= *kon; i++){
         vsota += i;
-=======
-// kazalec je v bistvu naslov
-
-// &r ... naslov od r
-// r ... vrednost 
-// *r ... vrednost na naslovu r
-// **r ... vrednost na naslovu *r
-
-// *niz je isto kot niz[i]
-// niz++            i++
-
-int vsota(int* zac, int* kon) {
-    int vsota = 0;
-    for(int *i = zac; i <= kon; i++){
-        vsota += *i;
->>>>>>> 0c5384eb34033e32100f168668581545832fc39a
     }
     return vsota;
 }
 
 void indeksInKazalec(int* t, int* indeks, int** kazalec) {
-<<<<<<< HEAD
-    // dopolnite ...
-}
-
-void frekvenceCrk(char* niz, int** frekvence) {
-    // dopolnite ...
-=======
     // t kaže na začetek tabele
     // indeks kaže na spremenljivko, v kateri bo indeks elementa v tabeli
     // kazalec kazalec kaže na kazalec, ki kaže na element v tabeli
@@ -66,22 +42,14 @@ void frekvenceCrk(char* niz, int** frekvence) {
     }
 
     *frekvence = pogostosti;
->>>>>>> 0c5384eb34033e32100f168668581545832fc39a
 }
 
 #ifndef test
 
 int main() {
-<<<<<<< HEAD
-    // koda za ro"cno testiranje (po "zelji)
-
-    
-=======
     
     int array[] = {1, 2, 3, 4, 5};
     printf("%d\n", vsota(array, array + 4));
-
->>>>>>> 0c5384eb34033e32100f168668581545832fc39a
 
     return 0;
 }
