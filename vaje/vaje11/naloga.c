89c09c25b5ed561c68e0bf97b6746fd0ddeec448
radov02 <radovicevic.erik1@gmail.com>
2024-05-14 09:10:01 +0200

    Vaje11

--- vaje/vaje11/naloga.c
+++ vaje/vaje11/naloga.c
@@ -7,23 +7,58 @@
 #include "naloga.h"
 
 int vsotaI(Vozlisce* zacetek) {
-    // popravite / dopolnite ...
-    return -999;
+    Vozlisce* kopija = zacetek;
+    int vsota = 0;
+
+    while(kopija != NULL){
+        vsota += kopija->podatek;
+        kopija = kopija->naslednje;
+    }
+    
+    return vsota;
 }
 
 int vsotaR(Vozlisce* zacetek) {
-    // popravite / dopolnite ...
-    return -999;
+    if(zacetek == NULL){
+        return 0;
+    }
+    if(zacetek->naslednje == NULL){
+        return zacetek->podatek;
+    }
+    
+    return zacetek->podatek + vsotaR(zacetek->naslednje);
 }
 
 Vozlisce* vstaviUrejenoI(Vozlisce* zacetek, int element) {
-    // popravite / dopolnite ...
-    return NULL;
+    Vozlisce* novo = malloc(sizeof(Vozlisce));
+    novo->podatek = element;
+    novo->naslednje = NULL;
+
+    if(zacetek == NULL || zacetek->podatek >= element) {    // ce je seznam prazen ali je že začetni večji od novega
+        novo->naslednje = zacetek;
+        return novo;
+    }
+
+    Vozlisce* kopija = zacetek;
+    while(kopija->naslednje != NULL && kopija->naslednje->podatek < element) {
+        kopija = kopija->naslednje;
+    }
+    novo->naslednje = kopija->naslednje;
+    kopija->naslednje = novo;
+    return zacetek;
 }
 
 Vozlisce* vstaviUrejenoR(Vozlisce* zacetek, int element) {
-    // popravite / dopolnite ...
-    return NULL;
+
+    if(zacetek == NULL || element <= zacetek->podatek){
+        Vozlisce* novo = malloc(sizeof(Vozlisce));
+        novo->podatek = element;
+        novo->naslednje = zacetek;
+        return novo;
+    }
+
+    zacetek->naslednje = vstaviUrejenoR(zacetek->naslednje, element);
+    return zacetek;
 }
 
 #ifndef test
