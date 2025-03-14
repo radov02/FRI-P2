e2dac6debcfa774395418a90fc3b696f9a79d491
radov02 <radovicevic.erik1@gmail.com>
2025-03-14 17:53:34 +0100

    Last commit

---
+++ dodatneNaloge/dn07/snake.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_KOORDINATA 1000000
+#define MIN_KOORDINATA -1000000
+
+int pozicijaKace[][];
+
+int main(){
+
+    int dolzinaKace = 1;
+    int xkace = 0, ykace = 0;
+    int smer = 3;
+
+    for(int korak = 0; korak < 10; korak++){
+
+    }
+
+
+    return 0;
+}
